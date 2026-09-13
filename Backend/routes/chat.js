750e00e451a37df16c647196a279177d4cf41eaa
abhinabadas02-TS <abhinabadas02@gmail.com>
2026-09-13 18:03:44 +0530

    Backend render push

--- Backend/routes/chat.js
+++ Backend/routes/chat.js
@@ -1,7 +1,7 @@
 import express from "express";
 import rateLimit from "express-rate-limit";
 import { chatWithAI, getChatLogs } from "../controllers/chatController.js";
-import adminAuth from "../middleware/adminAuth.js";
+import adminAuth from "../middleware/AdminAuth.js";
 
 const router = express.Router();
 
